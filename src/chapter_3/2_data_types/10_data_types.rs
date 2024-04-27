25cc51dee453067a9532876459226bd1d25fbb54
Mateusz Kopko <kopko.mateusz@gmail.com>
2024-04-27 13:29:50 +0200

    add more scripts

--- src/chapter_3/2_data_types/10_data_types.rs
+++ src/chapter_3/2_data_types/10_data_types.rs
@@ -6,11 +6,4 @@ fn main() {
     // array indexes are starting from 0
     let first = a[0];
     let second = a[1];
-
-    println!("first {first}");
-    println!("second {second}");
-
-    // this throws an error
-    let value = a[5];
-    println!("value {value}");
 }
--- src/chapter_3/2_data_types/9_data_types.rs
+++ src/chapter_3/2_data_types/9_data_types.rs
@@ -1,2 +1,29 @@
 fn main() {
+    // array type
+    // values in array must be the same type
+    // array has a fixed size
+
+    let a = [1, 2, 3, 4, 5];
+
+    // array will be most useful when we know the exact number of elements
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+
+    // specifying type of an array and number of elements
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // will create array 5 times value 3
+    let a = [3; 5];
 }
