875e94b27029a616d4edc2efe10b1dfe0914a606
Mateusz Kopko <kopko.mateusz@gmail.com>
2024-05-14 00:11:53 +0200

    add some code

---
+++ src/chapter_8/2_Storing_UTF_8_encoded_text_with_strings/1_example.rs
@@ -0,0 +1,5 @@
+// Many of the same operations available with Vec<T> are available with String as well, because String is actually implemented as a wrapper around a vector of bytes with some extra guarantees, restrictions, and capabilities
+
+fn main() {
+    let mut s = String::new();
+}
