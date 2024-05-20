ba22dc29b391ec96be33735564fa3efc42826208
Mateusz Kopko <kopko.mateusz@gmail.com>
2024-05-20 19:53:13 +0200

    add more learning scripts

---
+++ src/chapter_9/2_recoverable_rrrors_with_result/10_example.rs
@@ -0,0 +1,8 @@
+fn main() {
+    use std::fs;
+    use std::io;
+
+    fn read_username_from_file() -> Result<String, io::Error> {
+        fs::read_to_string("hello.txt")
+    }
+}
---
+++ src/chapter_9/2_recoverable_rrrors_with_result/9_example.rs
@@ -0,0 +1,12 @@
+fn main() {
+    use std::fs::File;
+    use std::io::{self, Read};
+
+    fn read_username_from_file() -> Result<String, io::Error> {
+        let mut username = String::new();
+
+        File::open("hello.txt")?.read_to_string(&mut username)?;
+
+        Ok(username)
+    }
+}
