644aeba48daf9218927faf95befae872a5b48902
Mateusz Kopko <kopko.mateusz@gmail.com>
2024-05-21 19:40:26 +0200

    more learning scripts

---
+++ src/chapter_9/3_to_panic_or_not_to_panic/1_example.rs
@@ -0,0 +1,16 @@
+loop {
+    // --snip--
+
+    let guess: i32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => continue,
+    };
+
+    if guess < 1 || guess > 100 {
+        println!("The secret number will be between 1 and 100.");
+        continue;
+    }
+
+    match guess.cmp(&secret_number) {
+        // --snip--
+}
